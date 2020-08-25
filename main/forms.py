b0f8977f3c27c1baa5eac0a6452023aa1f167373
Bebico <razvan.gheorfi@ro.maxbetgroup.com>
2020-08-25 23:36:26 +0300

    added image fields and bio for users

--- main/forms.py
+++ main/forms.py
@@ -13,7 +13,7 @@ for item in choices:
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'author', 'category', 'body', 'snippet')
+        fields = ('title', 'author', 'category', 'header_image', 'body', 'snippet')
 
         widgets = {
             'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter post title'}),
