951b12d9a8a892807b242aba2035dab2326def9c
Razvan <razvan.gheorfi@gmail.com>
2020-08-30 20:57:58 +0300

    Build A Blog Comment Section

--- main/models.py
+++ main/models.py
@@ -34,7 +34,6 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     header_image = models.ImageField(null=True, blank=True, upload_to="images/")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    #body = models.TextField()
     body = RichTextField(blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=255, default='coding')
@@ -50,3 +49,12 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
